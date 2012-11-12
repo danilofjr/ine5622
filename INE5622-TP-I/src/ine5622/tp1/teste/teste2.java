db3dbe507747eaffdd86c61a499b34fb28ec4fe8
Ariane <Ariane@Ariane-VAIO>
2012-11-12 21:00:25 -0200

    teste2

---
+++ INE5622-TP-I/src/ine5622/tp1/teste/teste2.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ine5622.tp1.teste;
+
+/**
+ *
+ * @author Ariane
+ */
+public class teste2 {
+    
+}
